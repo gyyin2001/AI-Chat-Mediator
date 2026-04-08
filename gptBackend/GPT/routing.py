6959130ea4522e0a41037c2b0d0d9a546de5b5db
gyyin2001 <johnnyyy1107zz@gmail.com>
2026-04-08 16:39:54 +0800

    Delete gptBackend directory

--- gptBackend/GPT/routing.py
+++
@@ -1,5 +0,0 @@
-from django.urls import re_path
-from . import consumers
-websocket_urlpatterns = [
-    re_path(r'ws/realtime/(?P<session_id>[-\w]+)/$', consumers.RealtimeRelayConsumer.as_asgi()),
-]
